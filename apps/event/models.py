bb51f5d2d7b01d24cf58736d8a70530ded7c3c44
Sebastian Narvaez Lopera <narvaezsebas8@gmail.com>
2023-07-25 23:09:19 -0500

    [add] Creacion inicial de los modelos utilizados para administrar la app event

--- apps/event/models.py
+++ apps/event/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+class EventType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Tipo de Evento"
+        verbose_name_plural = "Tipos de Eventos"
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.name
+
+class EventStatus(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Estado del Evento"
+        verbose_name_plural = "Estados de los eventos"
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.name
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=255)
+    date = models.DateField()
+    management = models.BooleanField()
+    type = models.ForeignKey(EventType, on_delete=models.DO_NOTHING)
+    status = models.ForeignKey(EventStatus, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        verbose_name = "Evento"
+        verbose_name_plural = "Eventos"
+        ordering = ['-id']
