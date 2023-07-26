d2e30a7367dfb9f26814466cd6cb97a55e12c9dd
Sebastian Narvaez Lopera <narvaezsebas8@gmail.com>
2023-07-25 22:48:28 -0500

    [add] Añadiendo los archivos iniciales del proyecto: Configuraciones generales, variables de entorno, conexión a Postgresql, segmentación del archivo de configuraciones y creación del directorio apps con la primera app event

---
+++ eventanywhere/settings/local.py
@@ -0,0 +1,30 @@
+import os
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DBPG_NAME'),
+        'USER': env('DBPG_USER'),
+        'PASSWORD': env('DBPG_PASS'),
+        'HOST': env('DBPG_HOST'),
+        'PORT': env('DBPG_PORT'),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
